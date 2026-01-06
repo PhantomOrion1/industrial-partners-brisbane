c30247ad85275ea4fba79156c03ed32dd40aef1b
Julian D'Urso <juju@Jumunji.local>
2026-01-06 21:27:05 +1100

    Adjust Macquarie logo order and speed up client scroll

--- src/components/ClientLogos.tsx
+++ src/components/ClientLogos.tsx
@@ -3,14 +3,14 @@ import { basePath } from '@/utils/path';
 const ClientLogos = () => {
   const clients = [
     { name: "China CITIC Bank", logo: `${basePath}601998.SS_BIG-0099d861.png`, size: "h-10 md:h-16" },
+    { name: "Macquarie", logo: `${basePath}MacquarieLogo.png`, size: "h-10 md:h-16" },
     { name: "AGL Energy", logo: `${basePath}AGL_Energy_logo.svg`, size: "h-12 md:h-18" },
     { name: "BCI Bus", logo: `${basePath}BCI_Bus_logo.svg.png`, size: "h-10 md:h-16" },
     { name: "Cleanaway", logo: `${basePath}cleanaway-logo-svg-vector.svg`, size: "h-8 md:h-14" },
     { name: "CPE Capital", logo: `${basePath}CPEcapital (1).png`, size: "h-10 md:h-16" },
     { name: "Opal Australian Paper", logo: `${basePath}opal_australian paper master_purple1_rgb.png`, size: "h-8 md:h-14" },
     { name: "United Heavy", logo: `${basePath}UnitedHeavy (1).png`, size: "h-10 md:h-16" },
-    { name: "Veolia", logo: `${basePath}Veolia_logo.svg.png`, size: "h-10 md:h-16" },
-    { name: "Macquarie", logo: `${basePath}MacquarieLogo.png`, size: "h-10 md:h-16" }
+    { name: "Veolia", logo: `${basePath}Veolia_logo.svg.png`, size: "h-10 md:h-16" }
   ];
 
   // Create multiple cycles for seamless infinite loop
